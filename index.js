a8974cc57effc7ebcc52c51734bd8f603be312f1
Fitim <fitimvata28@gmail.com>
2020-08-14 14:32:24 +0200

    Add prittier

--- index.js
+++ index.js
@@ -1,65 +1,72 @@
 #!/usr/bin/env node
 const yargs = require("yargs");
 const chalk = require("chalk");
-const boxen = require("boxen");
-const clear = require('clear');
-const figlet = require('figlet');
-const files = require('./lib/files');
-const inquirer = require('./lib/inquirer');
-const app = require('./lib/app');
-
+const clear = require("clear");
+const figlet = require("figlet");
+const files = require("./lib/files");
+const inquirer = require("./lib/inquirer");
+const app = require("./lib/app");
 
 clear();
 
 console.log(
-  chalk.yellow(
-    figlet.textSync('Trello Report', { horizontalLayout: 'full' })
-  )
+  chalk.yellow(figlet.textSync("Trello Report", { horizontalLayout: "full" }))
 );
-
 
 yargs
   .scriptName("trello-report")
-  .usage('$0 <cmd> [args]')
-  .command('generate [spreadsheetId]', 'Generate Excel report', (yargs) => {
-    yargs.option('spreadsheetId', {
-      type: 'string',
-      describe: 'Google Spread Sheet Id',
-      demandOption: true,
-    })
-    yargs.option('type', {
-      type: 'string',
-      default: 'by-month',
-      choices:  ['by-month', 'by-user', 'all'],
-      describe: 'Spreadsheet report type',
-    })
-  }, async function (argv) {
-    console.log("Generating report...")
-    await app.maybeSetup()
-    await app.generateSpreadsheet(argv.spreadsheetId,  argv.type)
-    console.log("DONE!");
-  })
+  .usage("$0 <cmd> [args]")
+  .command(
+    "generate [spreadsheetId]",
+    "Generate Excel report",
+    (yargs) => {
+      yargs.option("spreadsheetId", {
+        type: "string",
+        describe: "Google Spread Sheet Id",
+        demandOption: true,
+      });
+      yargs.option("type", {
+        type: "string",
+        default: "by-month",
+        choices: ["by-month", "by-user", "all"],
+        describe: "Spreadsheet report type",
+      });
+    },
+    async function (argv) {
+      console.log("Generating report...");
+      await app.maybeSetup();
+      await app.generateSpreadsheet(argv.spreadsheetId, argv.type);
+      console.log("DONE!");
+    }
+  )
 
-  .command('auto-resize [spreadsheetId]', 'Autoresize Excel report', (yargs) => {
-    yargs.option('spreadsheetId', {
-      type: 'string',
-      describe: 'Google Spread Sheet Id',
-      demandOption: true,
-    })
-  }, async function (argv) {
-    console.log("Autoresize report...")
-    await app.maybeSetup()
-    await app.autoResizeSpreadsheet(argv.spreadsheetId)
-    console.log("DONE!");
-  })
+  .command(
+    "auto-resize [spreadsheetId]",
+    "Autoresize Excel report",
+    (yargs) => {
+      yargs.option("spreadsheetId", {
+        type: "string",
+        describe: "Google Spread Sheet Id",
+        demandOption: true,
+      });
+    },
+    async function (argv) {
+      console.log("Autoresize report...");
+      await app.maybeSetup();
+      await app.autoResizeSpreadsheet(argv.spreadsheetId);
+      console.log("DONE!");
+    }
+  )
 
-  .command('auth google', 'Get Google token', (yargs) => {
-  }, async function (argv) {
-    await app.reAuthenticateGoogle()
-    console.log("DONE!");
-  })
+  .command(
+    "auth google",
+    "Get Google token",
+    (yargs) => {},
+    async function (argv) {
+      await app.reAuthenticateGoogle();
+      console.log("DONE!");
+    }
+  )
 
-  
-  .demandCommand(1, 'You need at least one command before moving on')
-  .help()
-  .argv
+  .demandCommand(1, "You need at least one command before moving on")
+  .help().argv;
